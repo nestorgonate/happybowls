6bc4d2a900ab10c7e9a4884cbce11acfb4b9acba
nestorgonate <negavilaneson@uide.edu.ec>
2026-03-23 14:12:49 -0500

    Show schedulers logs

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ import os
 from core import config
 from fastapi import FastAPI
 config.setupenv()
-from tasks.backgroundtask import StartScheduler
 from endpoints import agente
 from endpoints import dashboard
 from fastapi.middleware.cors import CORSMiddleware
@@ -14,13 +13,30 @@ import multiprocessing
 import logging
 from core.logs import setupLogs
 from endpoints import emailclient
+import asyncio
+from core.dependencies import getLoadEmailService
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime, timedelta, timezone
 setupLogs()
+loadEmails = getLoadEmailService()
 logger = logging.getLogger("email_organizer")
+base_dir = Path(__file__).resolve().parent
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await StartScheduler()
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(
+        loadEmails.checkEmails,
+        trigger="interval",
+        minutes=5,
+        max_instances=1,
+        id="emailService_handleEmails",
+        next_run_time=datetime.now(timezone.utc) + timedelta(seconds=1)
+    )
+    scheduler.start()
+    for job in scheduler.get_jobs():
+        logger.info(f"ID: {job.id} Tasks: {job.next_run_time}")
     yield
-base_dir = Path(__file__).resolve().parent
+    scheduler.shutdown()
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=agente.router)
 app.include_router(router=dashboard.router)
@@ -33,8 +49,10 @@ app.add_middleware(
     allow_methods=["POST", "GET", "DELETE", "UPDATE"],
     allow_headers=["*"],
 )
-if __name__ == "__main__":
+def main():
     multiprocessing.freeze_support()
     logger.info(msg="You can access your website at http://127.0.0.1:8000")
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app=app, host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    main()
